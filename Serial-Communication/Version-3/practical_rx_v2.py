1aa6858c77147b8f39012d09cf18c224c14b0fee
Anthony Coipel <99366232+Coipel@users.noreply.github.com>
2023-01-14 19:58:32 -0800

    Add files via upload

---
+++ Serial-Communication/Version-3/practical_rx_v2.py
@@ -0,0 +1,62 @@
+# [NOTE] This program runs on a personal computer not on the Arduino despite some variables refering to the Arduino
+# This program must be ran first before the Arduino button to send data is pushed (also have serial monitor closed in Arduino IDE)
+
+import serial
+import matplotlib.pyplot as pyplot
+
+msg_array = []
+total_strain_data = []
+total_valve_data = []
+total_time_data = []
+
+connection = serial.Serial('COM3', 57600) # A decently high baudrate is needed so the Arduino doesn't take so long to trasmit data that it leaves large gaps data collection
+while True:
+    while connection.in_waiting == 0:
+        pass
+    
+    msg = connection.readline().decode().rstrip('\r\n')
+    
+    if msg != "END" and msg != "OVER":    # Preventing the flags "END" and "OVER" from entering the payload
+        print(f"Gathered Data '{msg}'")
+        msg_array.append(msg)
+    
+    if msg == "END":
+        print("Filtering Data...")
+        
+        # Finding indices of desired data
+        strain_data_pointer = msg_array.index('SD') + 1
+        valve_data_pointer = msg_array.index('VD') + 1
+        time_data_pointer = msg_array.index('TD') + 1
+        
+        # Slicing out desired data from the message bundle
+        strain_data = msg_array[strain_data_pointer:valve_data_pointer-1]
+        valve_data = msg_array[valve_data_pointer:time_data_pointer-1]
+        time_data = msg_array[time_data_pointer:]
+        
+        # Accumulating the desired, filtered, data into a single array each
+        total_strain_data.extend(strain_data)
+        total_valve_data.extend(valve_data)
+        total_time_data.extend(time_data)
+        
+        # Clearing the message bundle for another round of messages
+        msg_array = []
+        print("Filtered Data")
+
+    if msg == "OVER":
+        print("Processing Data...")
+        break
+
+# Perform more intensive operations on data here (such as data conversion and storage)
+
+# Converts the list of strings representing numbers into actual int data type
+total_strain_data = list(map(int, total_strain_data))
+total_valve_data = list(map(int, total_valve_data))
+total_time_data = list(map(int, total_time_data))
+
+print(f"total_strain_data {total_strain_data}")
+print(f"total_valve_data {total_valve_data}")
+print(f"total_time_data {total_time_data}")
+
+pyplot.plot(total_time_data, total_strain_data)
+pyplot.grid(True)
+pyplot.show()
